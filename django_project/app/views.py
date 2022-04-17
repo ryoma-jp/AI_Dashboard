ce96a4214b22360c3aa89151b017d54ce3842e77
Ryoichi Matsumoto <iyo.rotomus@gmail.com>
2022-04-17 14:17:15 +0900

    show image file list on dataset detail page

--- django_project/app/views.py
+++ django_project/app/views.py
@@ -510,6 +510,12 @@ def dataset(request):
     """ Function: dataset
      * dataset top
     """
+    
+    # --- reset dataset detail parameters ---
+    if ('dropdown_dataset_info' in request.session.keys()):
+        del request.session['dropdown_dataset_info']
+        del request.session['selected_dataset_type']
+    
     if (request.method == 'POST'):
         if ('dataset_view_dropdown' in request.POST):
             request.session['dataset_view_dropdown_selected_project'] = request.POST.getlist('dataset_view_dropdown')[0]
@@ -573,10 +579,10 @@ def dataset_detail(request, project_id, dataset_id):
     dataset = get_object_or_404(Dataset, pk=dataset_id, project=project)
     
     if (request.method == 'POST'):
-        # logging.info('-------------------------------------')
-        # logging.info(request.POST)
-        # logging.info(request.POST.keys())
-        # logging.info('-------------------------------------')
+        logging.info('-------------------------------------')
+        logging.info(request.POST)
+        logging.info(request.POST.keys())
+        logging.info('-------------------------------------')
         
         if ('dataset_download' in request.POST.keys()):
             # --- dataset download ---
@@ -614,6 +620,10 @@ def dataset_detail(request, project_id, dataset_id):
                     'image_gallery_status': dataset.image_gallery_status,
                 }
                 return render(request, 'dataset_detail.html', context)
+            
+        if ('image_gallery_key' in request.POST.keys()):
+            selected_dataset_type = request.POST['image_gallery_key']
+            request.session['selected_dataset_type'] = selected_dataset_type
         
     # --- check dataset download ---
     if (dataset.download_status == dataset.STATUS_PREPARING):
@@ -641,10 +651,34 @@ def dataset_detail(request, project_id, dataset_id):
             dataset.image_gallery_status = dataset.STATUS_DONE
             dataset.save()
             
-        if (dataset.image_gallery_status == dataset.STATUS_DONE):
-            pass
-        
         selected_dataset_info = request.session.get('dropdown_dataset_info', None)
+        selected_dataset_type = request.session.get('selected_dataset_type', None)
+        
+        # --- set keys ---
+        image_gallery_keys = []
+        if (dataloader_obj.train_images is not None):
+            image_gallery_keys.append('train_images')
+        if (dataloader_obj.validation_images is not None):
+            image_gallery_keys.append('validation_images')
+        if (dataloader_obj.test_images is not None):
+            image_gallery_keys.append('test_images')
+        
+        # --- set image data file ---
+        image_gallery_data = []
+        if (selected_dataset_type is not None):
+            images_page_now = 0
+            images_per_page = 50
+            
+            json_file = os.path.join(download_dir, f'info_{selected_dataset_type}.json')
+            with open(json_file, 'r') as f:
+                json_data = json.load(f)
+            
+            for i in range(images_page_now, images_page_now+images_per_page):
+                image_gallery_data.append({
+                    'id': json_data['id'][i],
+                    'file': json_data['file'][i],
+                    'class_id': json_data['class_id'][i],
+                })
         
         context = {
             'text': get_version(),
@@ -655,6 +689,9 @@ def dataset_detail(request, project_id, dataset_id):
             'dataset_info': dataset_info,
             'selected_dataset_info': selected_dataset_info,
             'image_gallery_status': dataset.image_gallery_status,
+            'image_gallery_keys': image_gallery_keys,
+            'image_gallery_selected_item': selected_dataset_type,
+            'image_gallery_data': image_gallery_data,
         }
         return render(request, 'dataset_detail.html', context)
     else:
