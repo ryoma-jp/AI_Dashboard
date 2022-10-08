553791100e86f4285f22977468b1b79a7392cda5
Ryoichi Matsumoto <iyo.rotomus@gmail.com>
2022-10-08 16:31:12 +0900

    bugfix

--- django_project/app/machine_learning/lib/data_loader/data_loader.py
+++ django_project/app/machine_learning/lib/data_loader/data_loader.py
@@ -143,26 +143,26 @@ class DataLoaderCIFAR10(DataLoader):
 				save_file = self.file_download(dataset_dir, url)
 				
 				with tarfile.open(save_file) as tar:
-	def is_within_directory(directory, target):
-		
-		abs_directory = os.path.abspath(directory)
-		abs_target = os.path.abspath(target)
-	
-		prefix = os.path.commonprefix([abs_directory, abs_target])
-		
-		return prefix == abs_directory
-	
-	def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
-	
-		for member in tar.getmembers():
-			member_path = os.path.join(path, member.name)
-			if not is_within_directory(path, member_path):
-				raise Exception("Attempted Path Traversal in Tar File")
-	
-		tar.extractall(path, members, numeric_owner=numeric_owner) 
-		
-	
-	safe_extract(tar, path=dataset_dir)
+					def is_within_directory(directory, target):
+						
+						abs_directory = os.path.abspath(directory)
+						abs_target = os.path.abspath(target)
+					
+						prefix = os.path.commonprefix([abs_directory, abs_target])
+						
+						return prefix == abs_directory
+					
+					def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+					
+						for member in tar.getmembers():
+							member_path = os.path.join(path, member.name)
+							if not is_within_directory(path, member_path):
+								raise Exception("Attempted Path Traversal in Tar File")
+					
+						tar.extractall(path, members, numeric_owner=numeric_owner) 
+						
+					
+					safe_extract(tar, path=dataset_dir)
 			else:
 				logging.debug('CIFAR-10 dataset is exists (Skip Download)')
 		dataset_dir = os.path.join(dataset_dir, 'cifar-10-batches-py')
