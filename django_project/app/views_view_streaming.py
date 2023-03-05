ed30fb871d2b214ac8bf308bfc2611679755d920
Ryoichi MATSUMOTO <iyo.rotomus@gmail.com>
2023-03-05 12:03:00 +0900

    base code of inference model select

--- django_project/app/views_view_streaming.py
+++ django_project/app/views_view_streaming.py
@@ -33,6 +33,8 @@ def view_streaming(request):
         
         if ('streaming_interface_dropdown' in request.POST):
             request.session['streaming_interface'] = request.POST.getlist('streaming_interface_dropdown')[0]
+        elif ('streaming_model_dropdown' in request.POST):
+            request.session['streaming_model'] = request.POST.getlist('streaming_model_dropdown')[0]
         elif ('view_streaming_apply' in request.POST):
             ip_addr = [
                 request.POST['ip_0'],
@@ -69,24 +71,39 @@ def usb_cam(request):
      * USB Camera streaming
     """
     
-    def gen():
+    def gen(streaming_model):
+        """Generator
+        
+        This function is generator of byte frame for StreamingHttpResponse
+        
+        Args:
+            streaming_model (str): model name for inference
+        """
         cap = cv2.VideoCapture(0)
         if (not cap.isOpened()):
             # --- Retry ---
             cap.release()
-            time.sleep(3)
+            time.sleep(3)    # weight for complete ``cap.release`` (3sec is tentative)
             cap = cv2.VideoCapture(0)
         
         if (cap.isOpened()):
             cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
             cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
             cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+            cap.set(cv2.CAP_PROP_FPS, 30)
             
             while True:
                 # --- Get Frame ---
                 time_start = time.time()
                 ret, frame = cap.read()
                 overlay = frame.copy()
+                
+                # --- Infrerence ---
+                if (streaming_model == 'None'):
+                    text = f'Model: None'
+                    org = (320-100-5, 15)
+                    cv2.rectangle(overlay, (320-100, 0), (320, 20), (0, 0, 0), -1)
+                    cv2.putText(overlay, text, org, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=(225,250,0))
                 
                 # --- Put Text ---
                 time_end = time.time()
@@ -97,7 +114,7 @@ def usb_cam(request):
                 cv2.putText(overlay, text, org, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=(250,225,0))
                 
                 # --- Alpha and Concat---
-                alpha = 0.7
+                alpha = 0.8
                 frame = cv2.hconcat([frame, cv2.addWeighted(overlay, alpha, frame, 1-alpha, 0)])
                 
                 # --- Encode and Return byte frame ---
@@ -114,5 +131,7 @@ def usb_cam(request):
     logging.info(request.POST)
     logging.info('-------------------------------------')
     
-    return StreamingHttpResponse(gen(),
+    streaming_model = request.session.get('streaming_model', 'None')
+    
+    return StreamingHttpResponse(gen(streaming_model),
                content_type='multipart/x-mixed-replace; boundary=frame')
