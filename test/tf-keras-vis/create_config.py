c77bb1a1b78c52d018b4667c9c5a4c235f1996ca
Ryoichi MATSUMOTO <iyo.rotomus@gmail.com>
2023-06-11 08:31:24 +0900

    add MobileNetv2 and CenterNetHourGlass104, but doesn't work

--- test/tf-keras-vis/create_config.py
+++ test/tf-keras-vis/create_config.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 import json
+import tensorflow as tf
+import tensorflow_hub as hub
 
 from pathlib import Path
 from tensorflow.keras.applications import VGG16
@@ -9,8 +11,14 @@ def ArgParser():
     parser = argparse.ArgumentParser(description='Create config file for tf-keras-vis application',
                 formatter_class=argparse.RawTextHelpFormatter)
 
-    parser.add_argument('--model', dest='model', type=str, choices=['VGG16'], default='VGG16', \
-            help='Model')
+    model_list = [
+        'VGG16',
+        'MobileNetv2',
+        'CenterNetHourGlass104',
+    ]
+    parser.add_argument('--model', dest='model', type=str, choices=model_list, default='VGG16', \
+            help='Model\n'
+                 '  - MobileNetv2 and CenterNetHourGlass104 does not work')
     parser.add_argument('--output_dir', dest='output_dir', type=str, default='config', \
             help='Output directory')
 
@@ -28,8 +36,28 @@ def main():
     
     if (args.model == 'VGG16'):
         model = VGG16()
+    elif (args.model == 'MobileNetv2'):
+        model_url = 'https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/4'
+        image_shape = (224, 224)
+        
+        model = tf.keras.Sequential([
+            hub.KerasLayer(model_url, input_shape=image_shape+(3,), trainable=False)
+        ])
+    elif (args.model == 'CenterNetHourGlass104'):
+        model_url = 'https://tfhub.dev/tensorflow/centernet/hourglass_512x512/1'
+        image_shape = (512, 512)
+        dtype = tf.uint8
+        
+        model = tf.keras.Sequential([
+            hub.KerasLayer(model_url, input_shape=image_shape+(3,), trainable=False, dtype=dtype)
+        ])
+    else:
+        assert f'{args.model} is not supported'
+    
+    model.summary()
     
     for i, layer in enumerate(model.layers):
+        print(f'layer{i} config: {layer.get_config()}')
         if (layer.__class__.__name__ == 'Conv2D'):
             config = {
                 'model': args.model,
---
+++ test/tf-keras-vis/run_centernethourglass104.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+model="CenterNetHourGlass104"
+config_dir="config_${model}"
+
+python3 create_config.py --model ${model} --output_dir ${config_dir}
+exit
+files="${config_dir}/*"
+vis_dir="vis_result_${model}"
+for file in ${files}; do
+    echo ${file}
+    python3 tf-keras-vis-app.py --config ${file} --output_dir ${vis_dir}
+done
+
+
